781665c09948296a9d28cc9ecdecdc873a955d0a
ArtFED <temfedorenko@gmail.com>
2022-07-19 19:10:20 +0300

    carousel homework

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,31 @@ $(document).ready(function () {
       ]
    });
 });
+
+// tiny-slader
+
+// const slider = tns({
+//    container: '.carousel__inner',
+//    items: 1,
+//    slideBy: 'page',
+//    nav: false,
+//    controls: false,
+//    speed: 1200,
+//    responsive: {
+//       992: {
+//         edgePadding: 20,
+//         gutter: 20,
+//       },
+//       700: {
+//         gutter: 30
+//       }
+//     }
+//  });
+
+//  document.querySelector('.prev').addEventListener('click', function () {
+//    slider.goTo('prev');
+//  });
+
+//  document.querySelector('.next').addEventListener('click', function () {
+//    slider.goTo('next');
+//  });
