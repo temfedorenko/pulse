b29106996fab03b2d51bdafe3e0674184195ccca
ArtFED <temfedorenko@gmail.com>
2022-07-19 08:50:49 +0300

    homework3

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,16 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-        menuItem = document.querySelectorAll('.menu__item'),
-        hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+$(document).ready(function () {
+   $('.carousel__inner').slick({
+      speed: 1200,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               dots: true,
+               arrows: false
+            }
+         }
+      ]
+   });
+});
