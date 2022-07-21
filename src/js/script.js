8e53287072d268546ef9cbef6f92252cff95d4f4
ArtFED <temfedorenko@gmail.com>
2022-07-21 10:23:13 +0300

    modals hw2

--- src/js/script.js
+++ src/js/script.js
@@ -47,7 +47,40 @@ $(document).ready(function () {
          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
          $('.overlay, #order').fadeIn('slow');
       })
-   })
+   });
+
+   function validateForms(form) {
+      $(form).validate({
+         rules: {
+            name: {
+               required: true,
+               minlength: 2
+            },
+            phone: "required",
+            email: {
+               required: true,
+               email: true
+            }
+         },
+         messages: {
+            name: {
+               required: "Пожалуйста, введите свое имя",
+               minlength: jQuery.validator.format("Имя должно состоять минимум из {0} символов!")
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+               required: "Пожалуйста, введите свою почту",
+               email: "Неправильно введен адрес почты"
+            }
+         }
+      });
+   };
+
+   validateForms('#consultation-form');
+   validateForms('#consultation form');
+   validateForms('#order form');
+
+   $('input[name=phone]').mask("+7 (999) 999-99-99")
 
 });
 
