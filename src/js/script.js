e6da482a013fb5f20e2767ad527ca4a550820449
ArtFED <temfedorenko@gmail.com>
2022-07-25 18:25:54 +0300

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -102,7 +102,25 @@ $(document).ready(function () {
       return false;
    });
 
+   // Smooth scroll and pageUp
+
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+         $('.pageup').fadeIn();
+      } else $('.pageup').fadeOut();
+   });
+
+   $("a[href=#up]").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+   });
+
+   new WOW().init();
+
+
 });
+
 
 // tiny-slader
 
